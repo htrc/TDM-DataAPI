2deabbf30672477007b995c44d8045da1fa5e2ad
Boris Capitanu <borice@hotmail.com>
2018-10-31 10:30:31 -0500

    Added access logging

--- app/Filters.scala
+++ app/Filters.scala
@@ -1,10 +1,10 @@
 import javax.inject.{Inject, Singleton}
-
 import play.api.http.DefaultHttpFilters
 import play.filters.gzip.GzipFilter
-import v1.filters.InfoFilter
+import v1.filters.{InfoFilter, LoggingFilter}
 
 @Singleton
 class Filters @Inject()(gzipFilter: GzipFilter,
-                        buildInfoFilter: InfoFilter)
-  extends DefaultHttpFilters(gzipFilter, buildInfoFilter)
+                        buildInfoFilter: InfoFilter,
+                        accessLoggerFilter: LoggingFilter)
+  extends DefaultHttpFilters(gzipFilter, buildInfoFilter, accessLoggerFilter)
